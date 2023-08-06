dcd38bf43e88695d084e753054cb702b62171c80
ekawibawaputu <ekawibawaputu@gmail.com>
2023-08-07 05:29:42 +0800

    middleware dashboard, integration admin dashboard

--- resources/views/email/checkout/afterCheckout.blade.php
+++ resources/views/email/checkout/afterCheckout.blade.php
@@ -5,8 +5,8 @@ Hi, {{$checkout->User->name}}
 <br>
 Thank your for register on <b>{{$checkout->User->title}}</b>, please see payment instruction by click the button below.
 
-@component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id )])
-Get invoice
+@component('mail::button', ['url' => route('dashboard')])
+My Dashboard
 @endcomponent
 
 Thanks,<br>
